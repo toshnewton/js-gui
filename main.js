2943248b92ac9f3c8209aeeb56ca53c25a21e831
toshnewton <36524417+toshnewton@users.noreply.github.com>
2019-09-30 11:50:23 -0500

    getting closer lol

--- main.js
+++ main.js
@@ -1,51 +1,10 @@
-const x = Math.PI;
+const area = Math.PI;
 
-function area(y) {
-  return x * y * y;
+function areaOfCircle(radius) {
+  var calc = (area * (radius * radius));
+    alert(calc);
 }
 
 document.getElementById("result").innerHTML = area();
-
-
-
-
-
-const updateWithAdd = async (event) => {
-  document.querySelector('#result').innerHTML = ''
-    const regex = /[^a-zA-Z_]/g
-    const s = document.querySelector('#guest').value.replace(regex, '')
-    const x = parseInt(document.querySelector('#firstNumber').value)
-    const ans = `${s}, your answer is ${radius(x, x)}.`
-    document.querySelector('#result').innerHTML = ans
-}
-
-document.addEventListener('focusout', event => {
-  if ((event.target && event.target.id === 'firstNumber') ||
-    (event.target && event.target.id === 'secondNumber')) {
-    validate(event)
-  }
-})
-
-document.addEventListener('click', event => {
-  if (event.target && event.target.id === 'addButton') { updateWithAdd(event) }
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
